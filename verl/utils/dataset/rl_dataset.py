a6fbeea9da645ca95c0c21fe25689e8faf8c90a8
Franklin-Zhang0 <franklin.zhang@qq.com>
2025-04-08 15:01:41 +0000

    add text only dataset

--- verl/utils/dataset/rl_dataset.py
+++ verl/utils/dataset/rl_dataset.py
@@ -232,7 +232,7 @@ class RLHFDataset(Dataset):
             return state
         return self.__dict__.copy()
 
-class DummyJanusDPO_PairWise_RLHFDataset(Dataset):
+class JanusTextOnlyRLHFDataset(Dataset):
     """
     We assume the dataset contains a column that contains prompts and other information
     """
@@ -240,11 +240,11 @@ class DummyJanusDPO_PairWise_RLHFDataset(Dataset):
                  parquet_files: Union[str, List[str]],
                  tokenizer: PreTrainedTokenizer,
                  processor: Optional[ProcessorMixin] = None,
-                 prompt_key='prompt',
-                 image_key='images',
+                 prompt_key=None,
+                 image_key=None,
                  max_prompt_length=1024,
                  filter_prompts=True,
-                 cache_dir='~/.cache/verl/rlhf',
+                 cache_dir=None,
                  chat_template_func=None,
                  return_raw_chat=False,
                  truncation='error',
@@ -255,8 +255,6 @@ class DummyJanusDPO_PairWise_RLHFDataset(Dataset):
             parquet_files = [parquet_files]
 
         self.parquet_files = copy.deepcopy(parquet_files)
-        self.original_parquet_files = copy.deepcopy(parquet_files)  # use for resume
-        self.cache_dir = os.path.expanduser(cache_dir)
         self.tokenizer = tokenizer
         self.processor = processor
 
@@ -271,8 +269,18 @@ class DummyJanusDPO_PairWise_RLHFDataset(Dataset):
         self.filter_overlong_prompts = filter_overlong_prompts
         self.system_prompt = system_prompt
         
+        self.prompts = []
+        for i, parquet_file in enumerate(parquet_files):
+            self.parquet_files[i] = os.path.expanduser(parquet_file)
+            if self.parquet_files[i].endswith('.txt'):
+                with open(self.parquet_files[i], 'r') as f:
+                    prompts = f.readlines()
+                self.prompts.extend([prompt.strip() for prompt in prompts])
+            else:
+                raise ValueError(f"Unsupported file format: {self.parquet_files[i]}")
+        
     def __len__(self):
-        return 1024
+        return len(self.prompts)
 
     def __getitem__(self, item):
         """
@@ -280,33 +288,32 @@ class DummyJanusDPO_PairWise_RLHFDataset(Dataset):
         """
         row_dict = {}
 
-        chat = "dummy chat"
+        chat = [
+            {
+                "role": "<|User|>",
+                "content": self.prompts[item],
+            },
+            {"role": "<|Assistant|>", "content": ""},
+        ]
         
         sft_format = self.processor.apply_sft_template_for_multi_turn_prompts(
             conversations=chat,
             sft_format=self.processor.sft_format,
             system_prompt=self.system_prompt,
         )
+        prompt = sft_format + self.processor.image_start_tag
         
-        
-        raw_prompt = sft_format
+        raw_prompt = prompt
 
         is_multi_modal = False
-        assert not is_multi_modal, "JanusRLHFDataset only supports t2i data"
-        # expand image token
-        from PIL import Image
-        row_dict['multi_modal_data'] = {f'Good': [Image.fromarray(np.random.rand(3, 384, 384))], 'Bad': [Image.fromarray(np.random.rand(3, 384, 384))]}
-        image_inputs = {self.processor.image_processor(row_dict['multi_modal_data'][key], return_tensors='pt') for key in ['Good', 'Bad']}
+        assert not is_multi_modal, "JanusTextOnlyRLHFDataset only supports t2i data"
         
-        row_dict['multi_modal_output'] = {key: val for key, val in image_inputs.items()}
-        
-        input_ids, attention_mask = verl_F.tokenize_and_postprocess_data(prompt=sft_format,
+        input_ids, attention_mask = verl_F.tokenize_and_postprocess_data(prompt=prompt,
                                                                          tokenizer=self.tokenizer,
-                                                                         max_length=self.max_prompt_length - 1,
+                                                                         max_length=self.max_prompt_length,
                                                                          pad_token_id=self.tokenizer.pad_token_id,
                                                                          left_pad=True,
                                                                          truncation=self.truncation)
-
 
         input_ids = input_ids[0]
         attention_mask = attention_mask[0]
@@ -459,157 +466,6 @@ class DummyJanusDPORLHFDataset(Dataset):
         row_dict['input_ids'] = input_ids
         row_dict['attention_mask'] = attention_mask
         row_dict['position_ids'] = position_ids
-        row_dict['raw_prompt_ids'] = self.tokenizer.encode(raw_prompt, add_special_tokens=False)
-
-        # encode prompts without chat template
-        if self.return_raw_chat:
-            row_dict['raw_prompt'] = chat.tolist()
-
-        # add index for each prompt
-        index = row_dict.get("extra_info", {}).get("index", 0)
-        row_dict["index"] = index
-
-        return row_dict
-
-    def __getstate__(self):
-        if not self.serialize_dataset:
-            state = self.__dict__.copy()
-
-            if 'dataframe' in state:
-                del state['dataframe']
-            return state
-        return self.__dict__.copy()
-
-
-class JanusDPORLHFDataset(Dataset):
-    """
-    We assume the dataset contains a column that contains prompts and other information
-    """
-
-    def __init__(self,
-                 parquet_files: Union[str, List[str]],
-                 tokenizer: PreTrainedTokenizer,
-                 processor: Optional[ProcessorMixin] = None,
-                 prompt_key='prompt',
-                 image_key='images',
-                 max_prompt_length=1024,
-                 filter_prompts=True,
-                 cache_dir='~/.cache/verl/rlhf',
-                 chat_template_func=None,
-                 return_raw_chat=False,
-                 truncation='error',
-                 filter_overlong_prompts=False,
-                 system_prompt="",
-                 ):
-        if not isinstance(parquet_files, (List, ListConfig)):
-            parquet_files = [parquet_files]
-
-        self.parquet_files = copy.deepcopy(parquet_files)
-        self.original_parquet_files = copy.deepcopy(parquet_files)  # use for resume
-        self.cache_dir = os.path.expanduser(cache_dir)
-        self.tokenizer = tokenizer
-        self.processor = processor
-
-        self.prompt_key = prompt_key
-        self.image_key = image_key
-        self.max_prompt_length = max_prompt_length
-        self.filter_prompts = filter_prompts
-
-        self.return_raw_chat = return_raw_chat
-        self.chat_template_func = chat_template_func
-        self.truncation = truncation
-        self.filter_overlong_prompts = filter_overlong_prompts
-        self.system_prompt = system_prompt
-
-        # whether to store the dataset in state_dict()
-        # default not store
-        self.serialize_dataset = False
-        self.dataframe = InterleaveDataset(parquet_files)
-        
-
-    def _download(self, use_origin_parquet=False):
-        from verl.utils.fs import copy_to_local
-        parquet_files = self.parquet_files if not use_origin_parquet else self.original_parquet_files
-        for i, parquet_file in enumerate(parquet_files):
-            self.parquet_files[i] = copy_to_local(src=parquet_file, cache_dir=self.cache_dir)
-
-    def _read_files_and_tokenize(self):
-        dataframes = []
-        for parquet_file in self.parquet_files:
-            # read parquet files and cache
-            dataframe = pd.read_parquet(parquet_file)
-            dataframes.append(dataframe)
-        self.dataframe = pd.concat(dataframes)
-
-        print(f'dataset len: {len(self.dataframe)}')
-
-        # filter out too long prompts
-        if self.filter_overlong_prompts:
-            tokenizer = self.tokenizer
-            prompt_key = self.prompt_key
-            self.dataframe = self.dataframe[self.dataframe.apply(lambda doc: len(
-                tokenizer.apply_chat_template(doc[prompt_key], add_generation_prompt=True)) <= self.max_prompt_length,
-                                                                 axis=1)]
-
-            print(f'filter dataset len: {len(self.dataframe)}')
-
-    def resume_dataset_state(self):
-        self.serialize_dataset = False if hasattr(self, 'original_parquet_files') else True
-        # resume dataframe if not it's serialized in data.pt
-        if not self.serialize_dataset:
-            self._download(use_origin_parquet=True)  # download and resume from original parquet files
-            self._read_files_and_tokenize()
-        else:
-            print(r'old dataloader ckpt file is used, please train from scratch for better ckpt performance')
-
-    def __len__(self):
-        return len(self.dataframe)
-
-    def __getitem__(self, item):
-        """
-        Note that we also return the raw_input_ids so that it can be combined with other chat template
-        """
-        row_dict: dict = self.dataframe.iloc[item].to_dict()
-
-        chat = row_dict.pop(self.prompt_key)
-        
-        prompt_with_chat_template = self.processor.apply_sft_template_for_multi_turn_prompts(
-            conversations=chat,
-            sft_format=self.processor.sft_format,
-            system_prompt=self.system_prompt,
-        )
-        
-        raw_prompt = prompt_with_chat_template
-
-        is_multi_modal = f'{self.image_key}_0' in row_dict
-        assert is_multi_modal, "JanusRLHFDataset only supports multimodal data"
-        # expand image token
-        row_dict['multi_modal_data'] = {f'image_{i}': [process_image(f'{row_dict.pop(self.image_key)}_{i}')] for i in range(2)}
-        image_inputs = self.processor.image_processor(row_dict['multi_modal_data']['image'], return_tensors='pt')
-        
-        row_dict['multi_modal_inputs'] = {key: val for key, val in image_inputs.items()}
-        
-        input_ids, attention_mask = verl_F.tokenize_and_postprocess_data(prompt=prompt_with_chat_template,
-                                                                         tokenizer=self.tokenizer,
-                                                                         max_length=self.max_prompt_length,
-                                                                         pad_token_id=self.tokenizer.pad_token_id,
-                                                                         left_pad=True,
-                                                                         truncation=self.truncation)
-
-        if is_multi_modal:
-            from verl.models.transformers.qwen2_vl import get_rope_index
-
-            position_ids = get_rope_index(
-                self.processor,
-                input_ids=input_ids[0],
-                attention_mask=attention_mask[0],
-            )  # (3, seq_len)
-        else:
-            position_ids = compute_position_id_with_mask(attention_mask)
-
-        row_dict['input_ids'] = input_ids[0]
-        row_dict['attention_mask'] = attention_mask[0]
-        row_dict['position_ids'] = position_ids[0]
         row_dict['raw_prompt_ids'] = self.tokenizer.encode(raw_prompt, add_special_tokens=False)
 
         # encode prompts without chat template
