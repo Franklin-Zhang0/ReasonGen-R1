9327d03c52f4ce4d64f8276bfb14447a18c0a529
Franklin-Zhang0 <franklin.zhang@qq.com>
2025-04-10 20:41:54 +0000

    update test script

--- examples/test_janus.sh
+++ examples/test_janus.sh
@@ -5,6 +5,7 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 SYSTEM_PROMPT="Reason about how you should generate the image"
 
 GPUS=`nvidia-smi -L | wc -l`
+MODEL_PATH=deepseek-ai/Janus-Pro-7B  # replace it with your local file path
 RM_MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct
 RUN_NAME="test"
 export HYDRA_FULL_ERROR=1
@@ -14,23 +15,23 @@ python3 -m verl.trainer.image_generation_rl \
     algorithm.adv_estimator=dpo \
     data.train_files=/blob/franklin/datasets/Janus_RL/gen_eval/prompts.txt \
     data.system_prompt="$SYSTEM_PROMPT" \
-    data.train_batch_size=4 \
+    data.train_batch_size=32 \
     data.max_prompt_length=512 \
-    data.max_response_length=1024 \
+    data.max_response_length=600 \
     data.filter_overlong_prompts=True \
     data.truncation='error' \
     actor_rollout_ref.model.path=$MODEL_PATH \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=1e-5 \
     actor_rollout_ref.model.use_remove_padding=False \
-    actor_rollout_ref.actor.ppo_mini_batch_size=4 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.fsdp_config.param_offload=False \
-    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=hf \
@@ -39,7 +40,7 @@ python3 -m verl.trainer.image_generation_rl \
     actor_rollout_ref.model.cfg_weight=5.0 \
     actor_rollout_ref.model.detach_uncond=True \
     actor_rollout_ref.rollout.micro_batch_size=4 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
@@ -48,14 +49,15 @@ python3 -m verl.trainer.image_generation_rl \
     trainer.experiment_name=$RUN_NAME \
     trainer.n_gpus_per_node=4 \
     trainer.nnodes=1 \
-    trainer.save_freq=-1 \
+    trainer.save_freq=20 \
     trainer.test_freq=5 \
     trainer.total_epochs=15 \
+    trainer.resume_mode=disable \
     ++trainer.val_before_train=False \
     reward_model.reward_manager=image_generation \
     reward_model.model.path=$RM_MODEL_PATH \
-    reward_model.micro_batch_size_per_gpu=4 \
-    img_saving.save_freq=5 \
+    reward_model.micro_batch_size_per_gpu=16 \
+    img_saving.save_freq=5
     
 python ~/thinking.py > /dev/null 2>&1
 
