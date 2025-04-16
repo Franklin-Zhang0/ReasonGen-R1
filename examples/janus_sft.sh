d1e71fb88ee9f01bfc48c1c6450dfc4dd94d6a94
Franklin-Zhang0 <franklin.zhang@qq.com>
2025-04-16 18:33:53 +0000

    script update

--- examples/janus_sft.sh
+++ examples/janus_sft.sh
@@ -5,16 +5,17 @@ save_path=/home/v-zhangyu3/expdata/janus_sft/sft_test
 
 torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
      -m verl.trainer.img_rl_fsdp_sft_trainer \
-    data.train_files=/blob/franklin/datasets/Janus_RL/test/color \
-    data.val_files=/blob/franklin/datasets/Janus_RL/test/color \
-    data.train_batch_size=16 \
+    data.train_files=/blob/franklin/datasets/Janus_RL/sft_dataset/all_type@train \
+    data.val_files=/blob/franklin/datasets/Janus_RL/sft_dataset/all_type@test \
+    data.train_batch_size=256 \
     data.prompt_key=brief_caption \
     data.response_key=sft_prompt \
-    data.micro_batch_size_per_gpu=1 \
+    data.max_length=1536 \
+    data.micro_batch_size_per_gpu=4 \
     model.partial_pretrain=deepseek-ai/Janus-Pro-1B \
     trainer.default_local_dir=$save_path \
     trainer.project_name=sft_test \
     trainer.experiment_name=janus_sft \
-    trainer.total_epochs=20 \
+    trainer.total_epochs=4 \
     trainer.logger=['console'] \
     trainer.default_hdfs_dir=null $@
