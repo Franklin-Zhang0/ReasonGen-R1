3d985ff254f1ac44383f2740f011e612e2292600
Franklin-Zhang0 <franklin.zhang@qq.com>
2025-05-29 13:09:36 +0000

    update cot generate

--- Janus/cot_generate_inference.py
+++ Janus/cot_generate_inference.py
@@ -4,11 +4,12 @@ import torch
 import numpy as np
 from transformers import AutoModelForCausalLM
 from janus.models import MultiModalityCausalLM, VLChatProcessor
+from verl.utils.torch_functional import get_eos_mask
 
 
 # specify the path to the model
-model_path = "/home/v-zhangyu3/expdata/janus_sft/sft_test/global_step_40"
-vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained("deepseek-ai/Janus-Pro-7B")
+model_path = 'Franklin0/ReasonGen-R1'
+vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path, trust_remote_code=True)
 tokenizer = vl_chat_processor.tokenizer
 
 vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
@@ -16,8 +17,16 @@ vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
 )
 vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
 
-template = "A photo of {}. Reason in detail about how you can generate an image with the prompt above. Then generate the image."
-prompt = "an ornamental snowflake on a gray background"
+parallel_size = 16
+max_new_tokens = 1024
+
+output_path = "generated_samples"
+os.makedirs(output_path, exist_ok=True)
+
+
+template = "A photo of {}. Output a richly detailed prompt: "
+
+prompt = "A digitally rendered image of the iconic Monalisa capturing a selfie, boasting 8K resolution and hyper-realistic details that highlight the delicate textures of her skin and the intricate fibers of her clothing. The scene is illuminated with cinematic lighting that casts soft shadows and enhances the depth of field, giving a three-dimensional quality to the image. The background is blurred artfully, drawing full attention to her enigmatic expression and the modern device in her hands, all created with the precision of an Octane render engine."
 conversation = [
     {
         "role": "<|User|>",
@@ -37,12 +46,11 @@ input_ids = vl_chat_processor.tokenizer.encode(prompt)
 input_ids = torch.LongTensor(input_ids).cuda()
 attention_mask = torch.ones((len(input_ids)), dtype=torch.bool).cuda()
 do_sample = True
-max_new_tokens = 1024
 eos_token_id = vl_chat_processor.tokenizer.eos_token_id
 pad_token_id = vl_chat_processor.tokenizer.pad_token_id
 image_start_token_id = vl_chat_processor.image_start_id
-input_ids = input_ids.unsqueeze(0).repeat(4, 1)
-attention_mask = attention_mask.unsqueeze(0).repeat(4, 1)
+input_ids = input_ids.unsqueeze(0).repeat(parallel_size, 1)
+attention_mask = attention_mask.unsqueeze(0).repeat(parallel_size, 1)
 generation_config = {'cfg_weight': 5.0}
 output = vl_gpt.text_img_generate(
     input_ids,
@@ -54,14 +62,19 @@ output = vl_gpt.text_img_generate(
     image_start_token_id,
     generation_config,
 )
-
+sequence = output.sequences
+attn_mask = get_eos_mask(sequence, eos_token_id)
+res_len = attn_mask.sum(dim=-1)
+print("res_len", res_len)
 tokens = output.text_tokens
 for i in range(len(tokens)):
     cot = vl_chat_processor.tokenizer.decode(tokens[i], skip_special_tokens=False)
     print()
     print()
     print(cot)
-    os.makedirs('generated_samples', exist_ok=True)
-    save_path = os.path.join('generated_samples', f"img_cot_{i}.jpg")
+    with open(f"{output_path}/cot_{i}.txt", "w") as f:
+        f.write(cot)
+    print(f"Text saved to '{output_path}/cot_{i}.txt'")
+    save_path = os.path.join(f'{output_path}', "img_cot_{}.png".format(i))
     PIL.Image.fromarray(output.gen_img[i]).save(save_path)
-    print("Images saved to 'generated_samples' folder")
+print(f"Images saved to '{output_path}' folder")
